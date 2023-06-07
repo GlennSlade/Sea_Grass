4f6a80776e250113cbc9fc4c1832a2f12479ac6f
Slade <gs558@exeter.ac.uk>
2023-06-07 09:31:51 +0100

    updates

--- scripts/make_refl_stacks.R
+++ scripts/make_refl_stacks.R
@@ -18,8 +18,8 @@ library(writexl)
 #Philleigh 30m  stack
 
 # read in data
-Philleigh_30m_clipper <- read_sf(dsn = 'E:/Glenn/Philleigh/Processed/Philleigh_30m', layer = "Philleigh_30m_clip")
-Philleigh_30m_clip <- vect(Philleigh_30m_clipper)
+#Philleigh_30m_clipper <- read_sf(dsn = 'E:/Glenn/Philleigh/Processed/Philleigh_30m', layer = "Philleigh_30m_clip")
+#Philleigh_30m_clip <- vect(Philleigh_30m_clipper)
 
 Philleigh_30m_Blue_Refl <-  rast("E:/Glenn/Philleigh/Processed/Philleigh_30m/4_index/reflectance/Philleigh_30m_transparent_reflectance_blue.tif")
 Philleigh_30m_Green_Refl <-  rast("E:/Glenn/Philleigh/Processed/Philleigh_30m/4_index/reflectance/Philleigh_30m_transparent_reflectance_green.tif")
@@ -37,14 +37,14 @@ Philleigh_30m_ReflStack <- c(Philleigh_30m_Blue_Refl,Philleigh_30m_Green_Refl,Ph
 
 #write tif files
 
-#writeRaster(Philleigh_30m_ReflStack,"E:/Glenn/Botswana/ReflStacks/Philleigh_30m_Refl_Stack.tif")
-writeRaster(Philleigh_30m_ReflStack,"E:/Glenn/Botswana/ReflStacks/Philleigh_30m_Refl_Stack.tif", overwrite=TRUE)
+#writeRaster(Philleigh_30m_ReflStack,"E:/Glenn/Philleigh/R_Scripts/Sea_Grass/data_in/Philleigh_30m/Philleigh_30m_Refl_Stack.tif")
+writeRaster(Philleigh_30m_ReflStack,"E:/Glenn/Philleigh/R_Scripts/Sea_Grass/data_in/Philleigh_30m/Philleigh_30m_Refl_Stack.tif", overwrite=TRUE)
 
 #Philleigh 50m  stack
 
 # read in data
-Philleigh_50m_clipper <- read_sf(dsn = 'E:/Glenn/Philleigh/Processed/Philleigh_50m', layer = "Philleigh_50m_clip")
-Philleigh_50m_clip <- vect(Philleigh_50m_clipper)
+#Philleigh_50m_clipper <- read_sf(dsn = 'E:/Glenn/Philleigh/Processed/Philleigh_50m', layer = "Philleigh_50m_clip")
+#Philleigh_50m_clip <- vect(Philleigh_50m_clipper)
 
 Philleigh_50m_Blue_Refl <-  rast("E:/Glenn/Philleigh/Processed/Philleigh_50m/4_index/reflectance/Philleigh_50m_transparent_reflectance_blue.tif")
 Philleigh_50m_Green_Refl <-  rast("E:/Glenn/Philleigh/Processed/Philleigh_50m/4_index/reflectance/Philleigh_50m_transparent_reflectance_green.tif")
@@ -62,14 +62,14 @@ Philleigh_50m_ReflStack <- c(Philleigh_50m_Blue_Refl,Philleigh_50m_Green_Refl,Ph
 
 #write tif files
 
-#writeRaster(Philleigh_50m_ReflStack,"E:/Glenn/Botswana/ReflStacks/Philleigh_50m_Refl_Stack.tif")
-writeRaster(Philleigh_50m_ReflStack,"E:/Glenn/Botswana/ReflStacks/Philleigh_50m_Refl_Stack.tif", overwrite=TRUE)
+#writeRaster(Philleigh_50m_ReflStack,"E:/Glenn/Philleigh/R_Scripts/Sea_Grass/data_in/Philleigh_30m/Philleigh_50m_Refl_Stack.tif")
+writeRaster(Philleigh_50m_ReflStack,"E:/Glenn/Philleigh/R_Scripts/Sea_Grass/data_in/Philleigh_50m/Philleigh_50m_Refl_Stack.tif", overwrite=TRUE)
 
 #Philleigh 95m  stack
 
 # read in data
-Philleigh_95m_clipper <- read_sf(dsn = 'E:/Glenn/Philleigh/Processed/Philleigh_95m', layer = "Philleigh_95m_clip")
-Philleigh_95m_clip <- vect(Philleigh_95m_clipper)
+#Philleigh_95m_clipper <- read_sf(dsn = 'E:/Glenn/Philleigh/Processed/Philleigh_95m', layer = "Philleigh_95m_clip")
+#Philleigh_95m_clip <- vect(Philleigh_95m_clipper)
 
 Philleigh_95m_Blue_Refl <-  rast("E:/Glenn/Philleigh/Processed/Philleigh_95m/4_index/reflectance/Philleigh_95m_transparent_reflectance_blue.tif")
 Philleigh_95m_Green_Refl <-  rast("E:/Glenn/Philleigh/Processed/Philleigh_95m/4_index/reflectance/Philleigh_95m_transparent_reflectance_green.tif")
@@ -87,5 +87,5 @@ Philleigh_95m_ReflStack <- c(Philleigh_95m_Blue_Refl,Philleigh_95m_Green_Refl,Ph
 
 #write tif files
 
-#writeRaster(Philleigh_95m_ReflStack,"E:/Glenn/Botswana/ReflStacks/Philleigh_95m_Refl_Stack.tif")
-writeRaster(Philleigh_95m_ReflStack,"E:/Glenn/Botswana/ReflStacks/Philleigh_95m_Refl_Stack.tif", overwrite=TRUE)
+#writeRaster(Philleigh_95m_ReflStack,"E:/Glenn/Philleigh/R_Scripts/Sea_Grass/data_in/Philleigh_30m/Philleigh_95m_Refl_Stack.tif")
+writeRaster(Philleigh_95m_ReflStack,"E:/Glenn/Philleigh/R_Scripts/Sea_Grass/data_in/Philleigh_95m/Philleigh_95m_Refl_Stack.tif", overwrite=TRUE)
